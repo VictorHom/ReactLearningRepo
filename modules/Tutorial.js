745fa6c76269565d2e2d0213b0ad7de4d968ef06
Victor Hom <vh62@cornell.edu>
2016-03-01 22:16:39 -0500

    worked on tutorial from facebook react page

--- modules/Tutorial.js
+++ modules/Tutorial.js
@@ -1,9 +1,48 @@
 import React from 'react';
 
-var CommentBox = Reach.createClass({
-	render : function() {
-		<div className="commentBox">
-			Hello, world! I am a commentBox.
-		</div>
+export default React.createClass({
+	render : () => {
+		return (
+			<div className="commentBox">
+				<h1>Comments</h1>
+				<CommentList />
+				<CommentForm />
+			</div>
+		);
 	}
-})
+});
+
+
+let CommentForm = React.createClass({
+	render : () => {
+		return (
+			<div className="commentForm">
+				Hello world! I am a CommentForm!
+			</div>
+		);
+	}
+});
+
+let CommentList = React.createClass({
+	render : () => {
+		return (
+			<div className="commentList">
+				<Comment author="Pete Hunt">This is one comment</Comment>
+				<Comment author="Jordan Walke">This is another comment</Comment>
+			</div>
+		);
+	}
+});
+
+var Comment = React.createClass({
+  render: function() {
+    return (
+      <div className="comment">
+        <h2 className="commentAuthor">
+          {this.props.author}
+        </h2>
+        {this.props.children}
+      </div>
+    );
+  }
+});
